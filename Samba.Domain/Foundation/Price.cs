c7bfffcdc91d8f1997f2dd67f51a9d798f6fbed7
basio <m.elbasiouny@gmail.com>
2013-01-27 14:58:38 +0200

    inital commit

---
+++ Samba.Domain/Foundation/Price.cs
@@ -0,0 +1,26 @@
+﻿namespace Samba.Domain.Foundation
+{
+    public class Price
+    {
+        public Price()
+        {
+
+        }
+
+        public Price(decimal price, string currencyCode)
+        {
+            Amount = price;
+            //CurrencyCode = currencyCode;
+        }
+
+        public string CurrencyCode { get {return "";}
+            set { }
+        }
+        public decimal Amount { get; set; }
+
+        public override string ToString()
+        {
+            return Amount.ToString("#,#0.00 " + CurrencyCode);
+        }
+    }
+}
---
+++ Samba.Domain/Models/Suppliers/Supplier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Samba.Infrastructure.Data;
+
+namespace Samba.Domain.Models.Suppliers
+{
+    public class Supplier : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //  public string GroupCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Note { get; set; }
+        public DateTime AccountOpeningDate { get; set; }
+        public bool InternalAccount { get; set; }
+
+        private static readonly Supplier _null = new Supplier { Name = "*" };
+        public static Supplier Null { get { return _null; } }
+
+        public Supplier()
+        {
+            AccountOpeningDate = DateTime.Now;
+        }
+    }
+}
